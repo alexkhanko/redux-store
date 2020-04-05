e079579e1da7b44ff40cee2fe76e3499d8617086
Alexey <alex.hanko@mail.ru>
2020-04-05 15:14:51 +0300

    Updates

---
+++ precache-manifest.d04d736e4d29ccef6cf57840dc9e7a30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d4c349e867f7d3766ab",
+    "url": "/redux-store/static/css/main.eace434a.chunk.css"
+  },
+  {
+    "revision": "4d4c349e867f7d3766ab",
+    "url": "/redux-store/static/js/main.4d4c349e.chunk.js"
+  },
+  {
+    "revision": "89babbbe9832d51606e7",
+    "url": "/redux-store/static/js/1.89babbbe.chunk.js"
+  },
+  {
+    "revision": "4135fb482306d3c0809f",
+    "url": "/redux-store/static/js/runtime~main.4135fb48.js"
+  },
+  {
+    "revision": "9e9492f7a32bf88c76beb0c9744262d3",
+    "url": "/redux-store/index.html"
+  }
+];
